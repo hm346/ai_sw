c69650eb366a42ca80622234438f6ceef4483cf7
hm <3517873327@qq.com>
2026-05-31 21:20:02 +0800

    添加 ai/fde, ai/fe, ai/model_scope, wnd/prompt 文件

---
+++ ai/fe/js/es6/var_let_const/3.js
@@ -0,0 +1,24 @@
+
+//常量一开始就要赋值
+
+const key='abc123'
+let a; //undefined;
+let points=50;
+points=51;
+//let 不止值可以改变，类型也可以改变，但不建议这么做
+
+let winner='戴';
+const person={
+name:'李',
+age:18
+
+}
+
+person.age++
+console.log(person)
+
+
+
+
+
+
